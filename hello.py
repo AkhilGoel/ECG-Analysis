4642c9aee29a1a872adf7a2ad3a86aff27cae0e4
AkhilGoel <leosmith.goel58@gmail.com>
2016-05-03 21:28:57 +0530

    Added fft

--- hello.py
+++ hello.py
@@ -79,7 +79,7 @@ def fft():
                 inter.remove(i)
                 k = k+1
         arr_inerpolate = []
-        arr_interpolate = interpolate.spline(peaks,arrPeaks,intet,order=3,kind='smoothest',conds=None)
+        arr_interpolate = interpolate.spline(peaks,arrPeaks,inter,order=3,kind='smoothest',conds=None)
         ps = np.abs(np.fft.fft(arr_interpolate))**2
         time_step = 1/4
         freqs = np.fft.fftfreq(arr_interpolate.size,time_step)
